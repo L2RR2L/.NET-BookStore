df1aa442ba8d21a2c96e7de7788ff59be69debe4
Mehdi <mohamedmehdi.khelil@insat.ucar.tn>
2023-12-04 20:09:23 +0100

    intial commit

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Projet.Models;
+
+namespace Projet.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+
+    }
+}
---
+++ Models/Category.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Projet.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; } = "undefined";
+
+        [DisplayName("Display Order")]
+        [Range(1,50)]
+        public int DisplayOrder { get; set; }
+
+        public DateTime CreationTime { get; set; } = DateTime.Now;
+
+         
+    }
+}
